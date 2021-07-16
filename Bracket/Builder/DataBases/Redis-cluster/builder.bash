fec2f302a26416fc6eb95297f807b96e3c115ba5
Payne <wuzhipeng1289690157@gamil.com>
2021-07-16 18:14:02 +0800

    add create cluster script

--- Bracket/Builder/DataBases/Redis-cluster/builder.bash
+++ Bracket/Builder/DataBases/Redis-cluster/builder.bash
@@ -2,6 +2,16 @@
 # Prepare configuration file
 Redis_Port=(3220 3221 3222 3223 3224 3225 3226 3227 3228)
 
+# Get the first network card address
+if [[ $(uname -s) == "Darwin" ]]; then
+  ipaddrs=$(ifconfig en0 | grep -E "inet 1.*? netmask" | awk '{print $2}')
+elif [[ $(uname -s) == "Linux" ]]; then
+  ipaddrs=$(ifconfig eth0 | awk 'NR==2 {print $2}')
+else
+  echo "windows?"
+fi
+
+# make file dir
 mkdir redis_cluster_conf && cd redis_cluster_conf
 for element in "${Redis_Port[@]}"; do
   mkdir "${element}"
@@ -14,6 +24,7 @@ for element in "${Redis_Port[@]}"; do
 done
 cd ../
 
+# start redis server
 cat <<EOF | tee -a ./redis_cluster_conf/startServer.bash
 #!/bin/bash
 Redis_Port=(3220 3221 3222 3223 3224 3225 3226 3227 3228)
@@ -26,3 +37,14 @@ for element in "\${Redis_Port[@]}"; do
   cd ..
 done
 EOF
+
+# create cluster
+redis-cli --cluster create ${ipaddrs}:3220 \
+  ${ipaddrs}:3221 \
+  ${ipaddrs}:3222 \
+  ${ipaddrs}:3223 \
+  ${ipaddrs}:3224 \
+  ${ipaddrs}:3225 \
+  ${ipaddrs}:3226 \
+  ${ipaddrs}:3227 \
+  ${ipaddrs}:3228 -a 5by65aSn5peg5q+UcmVkaXM=
