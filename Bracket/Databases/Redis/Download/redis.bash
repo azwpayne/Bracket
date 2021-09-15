32979f020f8e9ecf7e8bfdb618e089a1dc3a102d
Payne <wuzhipeng1289690157@gamil.com>
2021-09-15 15:13:21 +0800

    push or fix some commit

--- Bracket/Databases/Redis/Download/redis.bash
+++ Bracket/Databases/Redis/Download/redis.bash
@@ -1,21 +1,24 @@
 #!/usr/bin/bash
 
-RedisVerison=6.2.5
+# Install single redis
+
+
+Redis_Version=6.2.5
 
 # download Redis
-wget "https://download.redis.io/releases/redis-${RedisVerison}.tar.gz"
+wget "https://download.redis.io/releases/redis-${Redis_Version}.tar.gz"
 
 ## create folder and  unzip Redis.tar.gz file
 mkdir -p /opt/redis/
-tar xzf redis-${RedisVerison}.tar.gz -C /opt/redis/
+tar xzf redis-${Redis_Version}.tar.gz -C /opt/redis/
 
 ## compile redis
-cd /opt/redis/redis-${RedisVerison} && make
+cd /opt/redis/redis-${Redis_Version} && make
 
 ## link and Configure environment variables of redis
-ln -s /opt/redis/redis-$RedisVerison/ /usr/local/redis
+ln -s /opt/redis/redis-${Redis_Version}/ /usr/local/redis
 echo "export PATH=/usr/local/redis/src:\$PATH" >> /etc/profile
 source /etc/profile
 
 ## rm Installation package
-rm -rf redis-${RedisVerison}.tar.gz
+rm -rf redis-${Redis_Version}.tar.gz
