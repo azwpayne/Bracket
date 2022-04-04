b40ea847f7edb0c7e5f49e145eb5c78818285e14
paynewu <wuzhipeng1289690157@gmail.com>
2022-04-04 10:11:04 +0800

    rm user to sudoer

--- ProgramEnv/Go/go.bash
+++ ProgramEnv/Go/go.bash
@@ -3,22 +3,16 @@
 # @email: wuzhipeng1289690157@gmail.com
 # Set up Go development environment
 
-VERSION=1.17.6
+VERSION=1.18
 PKG=go${VERSION}.linux-amd64.tar.gz
 SRC="https://golang.google.cn/dl/${PKG}"
 echo "Downloading ${SRC} ..."
 sudo wget -c "${SRC}"
 sudo tar -zxf "${PKG}" -C /usr/local/
-sudo cat >> /etc/profile <<EOF
+sudo cat <<EOF >>/etc/profile
 # config golang config
-export PATH="/usr/local/go/bin:\$PATH"
-export GOROOT=$GO_INSTALL_DIR/$GOVERSION # GOROOT 设置
-export GOPATH=$WORKSPACE/golang # GOPATH 设置
-export PATH=$GOROOT/bin:$GOPATH/bin:$PATH # 将 Go 语言自带的和通过 go install 安装的二进制文件加入到 PATH 路径中
 export GO111MODULE="on"
 export GOPROXY=https://goproxy.cn,direct # 安装 Go 模块时，代理服务器设置
-export GOPRIVATE=
-export GOSUMDB=off # 关闭校验 Go 依赖包的哈希值
 EOF
 
 
