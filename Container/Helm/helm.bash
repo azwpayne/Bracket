b2473eb4a3fb6198836291fc6aadcb9c45031d6c
payne-wu <wuzhipeng1289690157@gmail.com>
2022-07-19 13:55:25 +0800

    update helm repo source

--- Container/Helm/helm.bash
+++ Container/Helm/helm.bash
@@ -10,16 +10,17 @@ pkg=helm-v${version}-linux-amd64.tar.gz
 # download helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 # install it
-tar -zxvf ${pkg} &&
-  mv linux-amd64/helm /usr/local/bin/helm
+tar -zxvf ${pkg} && mv linux-amd64/helm /usr/local/bin/helm
 # add Commonly used repo
 helm repo add apphub https://apphub.aliyuncs.com/
 helm repo add bitnami https://charts.bitnami.com/bitnami/
 helm repo add azure https://mirror.azure.cn/kubernetes/charts/
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add stable https://charts.helm.sh/stable
-helm repo add googleapis https://kubernetes-charts.storage.googleapis.com
 helm repo add aliyuncs https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+helm repo add harbor https://helm.goharbor.io                   #harbor的
+helm repo add gpu-helm-charts https://nvidia.github.io/gpu-monitoring-tools/helm-charts    #NVIDIA DCGM的
+helm repo add elastic https://helm.elastic.co                   #elastic的 elasticsearch
 # update repo
 helm repo update
 # check helm version
